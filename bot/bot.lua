e06e270c54ed58584902f7befb3b6d72e45dfb5d
magamplus <3bqjzu@30mail.ir>
2016-01-08 16:49:53 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {160612764,176229065,135909309,54213663},
+    sudo_users = {76458212,95876809,179983320},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
